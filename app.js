d490a8930905b851e23296e5b2d22c28a6351f1e
ferdinandmwal123 <52549669+ferdinandmwal123@users.noreply.github.com>
2019-07-16 18:43:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("scroll-to-bottom").addEventListener("click", function () {
+  document.body.scrollIntoView(false);
+});
+
+document.getElementById('dwnlnk').style.top = '30px'
